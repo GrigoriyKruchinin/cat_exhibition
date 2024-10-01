d8ac2e465fd5b704aa4beb50b3ad091a557f94db
GrigoriyKruchinin <gkruchinin75@gmail.com>
2024-10-01 14:41:31 +0200

    added docstring for api endpoints

--- app/api/v1/breed.py
+++ app/api/v1/breed.py
@@ -10,16 +10,30 @@ breed_service = BreedService()
 
 @router.post("/", response_model=BreedSchema)
 def create_breed(breed_data: BreedCreateSchema, db: Session = Depends(get_db)):
+    """
+    Создает новую породу.
+
+    - **breed_data**: Данные для создания породы.
+    """
     return breed_service.create_breed(db, breed_data)
 
 
 @router.get("/", response_model=list[BreedSchema])
 def get_all_breeds(db: Session = Depends(get_db)):
+    """
+    Получает список всех пород.
+    """
     return breed_service.get_all(db)
 
 
 @router.get("/{breed_id}", response_model=BreedSchema)
 def get_breed(breed_id: int, db: Session = Depends(get_db)):
+    """
+    Получает информацию о породе по ее идентификатору.
+
+    - **breed_id**: Идентификатор породы.
+    - Возвращает 404, если порода не найдена.
+    """
     breed = breed_service.get_by_id(db, breed_id)
     if breed is None:
         raise HTTPException(status_code=404, detail="Breed not found")
@@ -30,6 +44,13 @@ def get_breed(breed_id: int, db: Session = Depends(get_db)):
 def update_breed(
     breed_id: int, breed_data: BreedCreateSchema, db: Session = Depends(get_db)
 ):
+    """
+    Обновляет данные породы по ее идентификатору.
+
+    - **breed_id**: Идентификатор породы.
+    - **breed_data**: Данные для обновления породы.
+    - Возвращает 404, если порода не найдена.
+    """
     breed = breed_service.get_by_id(db, breed_id)
     if breed is None:
         raise HTTPException(status_code=404, detail="Breed not found")
@@ -38,6 +59,12 @@ def update_breed(
 
 @router.delete("/{breed_id}", response_model=dict)
 def delete_breed(breed_id: int, db: Session = Depends(get_db)):
+    """
+    Удаляет породу по ее идентификатору.
+
+    - **breed_id**: Идентификатор породы.
+    - Возвращает 404, если порода не найдена.
+    """
     breed = breed_service.delete(db, breed_id)
     if breed is None:
         raise HTTPException(status_code=404, detail="Breed not found")
--- app/api/v1/kitten.py
+++ app/api/v1/kitten.py
@@ -13,16 +13,30 @@ kitten_service = KittenService()
 
 @router.post("/", response_model=KittenSchema)
 def create_kitten(kitten_data: KittenCreateSchema, db: Session = Depends(get_db)):
+    """
+    Создает нового котенка.
+
+    - **kitten_data**: Данные для создания котенка.
+    """
     return kitten_service.create_kitten(db, kitten_data)
 
 
 @router.get("/", response_model=list[KittenSchema])
 def get_all_kittens(db: Session = Depends(get_db)):
+    """
+    Получает список всех котят.
+    """
     return kitten_service.get_all(db)
 
 
 @router.get("/{kitten_id}", response_model=KittenSchema)
 def get_kitten(kitten_id: int, db: Session = Depends(get_db)):
+    """
+    Получает информацию о котенке по его идентификатору.
+
+    - **kitten_id**: Идентификатор котенка.
+    - Возвращает 404, если котенок не найден.
+    """
     kitten = kitten_service.get_by_id(db, kitten_id)
     if kitten is None:
         raise HTTPException(status_code=404, detail="Kitten not found")
@@ -33,6 +47,13 @@ def get_kitten(kitten_id: int, db: Session = Depends(get_db)):
 def update_kitten(
     kitten_id: int, kitten_data: KittenCreateSchema, db: Session = Depends(get_db)
 ):
+    """
+    Обновляет данные котенка по его идентификатору.
+
+    - **kitten_id**: Идентификатор котенка.
+    - **kitten_data**: Данные для обновления котенка.
+    - Возвращает 404, если котенок не найден.
+    """
     kitten = kitten_service.get_by_id(db, kitten_id)
     if kitten is None:
         raise HTTPException(status_code=404, detail="Kitten not found")
@@ -41,6 +62,12 @@ def update_kitten(
 
 @router.delete("/{kitten_id}", response_model=dict)
 def delete_kitten(kitten_id: int, db: Session = Depends(get_db)):
+    """
+    Удаляет котенка по его идентификатору.
+
+    - **kitten_id**: Идентификатор котенка.
+    - Возвращает 404, если котенок не найден.
+    """
     kitten = kitten_service.delete(db, kitten_id)
     if kitten is None:
         raise HTTPException(status_code=404, detail="Kitten not found")
@@ -49,4 +76,9 @@ def delete_kitten(kitten_id: int, db: Session = Depends(get_db)):
 
 @router.get("/breed/{breed_id}", response_model=list[KittenSchema])
 def get_kittens_by_breed(breed_id: int, db: Session = Depends(get_db)):
+    """
+    Получает список котят по идентификатору породы.
+
+    - **breed_id**: Идентификатор породы.
+    """
     return kitten_service.get_kittens_by_breed(db, breed_id)
